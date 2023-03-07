4cab2241f619c2e8a4d0fce84f28a8f063d4f046
Michael Rulle <michael@rulle.xyz>
2023-03-07 12:22:58 +0100

    pushed the rest of changes

---
+++ src/ApplicationCore/Entities/Coupon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Entities;
+public class Coupon : BaseEntity, IAggregateRoot
+{
+    public Coupon() { }
+    public Coupon(int id, string name, int percentageDiscount, DateTime startDate, DateTime endDate)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+        this.PercentageDiscount = percentageDiscount;
+    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PercentageDiscount {  get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
