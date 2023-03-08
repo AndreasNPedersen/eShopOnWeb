4d61d26998bb887c5c018c87cbbc297a47b9829b
michael <michael@rulle.xyz>
2023-03-08 23:30:10 +0100

    added postvalidatecoupon

---
+++ src/PublicApi/CouponEndpoints/GetByNameCouponRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.eShopWeb.PublicApi.CouponEndpoints;
+
+public class GetByNameCouponRequest : BaseRequest
+{
+    public string CouponName { get; init; }
+
+    public GetByNameCouponRequest(string couponName)
+    {
+        CouponName = couponName;
+    }
+}
