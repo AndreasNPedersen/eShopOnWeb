e94e1f84e7cdd1e9ddbd44db7ad36958ea8d0ba6
michael <michael@rulle.xyz>
2023-05-21 15:22:54 +0200

    added tests for deleting coupons, and test for coupon not found on getcoupon

--- tests/PublicApiIntegrationTests/CouponEndpoints/GetCouponEndpointTest.cs
+++ tests/PublicApiIntegrationTests/CouponEndpoints/GetCouponEndpointTest.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Xunit;
 using Microsoft.eShopWeb.PublicApi.CouponEndpoints.GetCoupons;
 using Microsoft.eShopWeb;
 using Microsoft.eShopWeb.PublicApi.CouponEndpoints.AddCoupons;
@@ -15,48 +14,68 @@ using System.Net.Http;
 using System.Runtime.CompilerServices;
 using Azure.Core;
 using System.Text.Json;
+using Microsoft.eShopWeb.PublicApi.CouponEndpoints;
+using System.Net;
 
 namespace PublicApiIntegrationTests.CouponEndpoints;
 
-public class GetCouponEndpointTest : IAsyncLifetime
-{
-    public GetCouponEndpointTest() {
-        
-    }
 
-    [Fact(DisplayName = "Successful getCouponRequest")]
-    public async void testGetCoupon()
+[TestClass]
+public class GetCouponEndpointTest 
+{
+    public int couponId = 1;
+    
+    [TestMethod]
+    public async Task testGetCoupon()
     {
-        var response = await ProgramTest.NewClient.GetAsync("api/coupons/1");
+        //var jsonContent = GetValidNewItemJson();
+        var token = ApiTokenHelper.GetAdminUserToken();
+        var client = ProgramTest.NewClient;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await client.GetAsync("api/coupon/" + couponId);
         response.EnsureSuccessStatusCode();
         var stringResponse = await response.Content.ReadAsStringAsync();
-        var model = stringResponse.FromJson<GetByIdCouponResponse>();
+        var model = stringResponse.FromJson<CouponDto>();
+        
 
         Assert.IsNotNull(model);
-        Assert.AreEqual(1, model.Coupon.Id);
-        Assert.AreEqual("MAY_10_PERCENT", model.Coupon.Name);
-
-
+        Assert.AreEqual(1, model.Id);
+        Assert.AreEqual("JUNE_10_PERCENT", model.Name);
     }
 
-    private async Task addCouponBeforeTest()
+
+    [TestMethod]
+    public async Task testFailureCouponNotFound()
+    {
+        int nonExistantCouponId = 101;
+        //var jsonContent = GetValidNewItemJson();
+        var token = ApiTokenHelper.GetAdminUserToken();
+        var client = ProgramTest.NewClient;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await client.GetAsync("api/coupon/" + nonExistantCouponId);
+
+        Assert.IsNotNull(response);
+        Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+
+
+    [TestInitialize]
+    public async Task addCouponBeforeTest()
     {
         AddCouponRequest request = new AddCouponRequest();
-        request.Name = "MAY_10_PERCENT";
+        request.Name = "JUNE_10_PERCENT";
         request.StartDate = DateTime.Now;
         request.EndDate = DateTime.Now.AddDays(10);
         request.PercentageDiscount = 10;
+
+        var token = ApiTokenHelper.GetAdminUserToken();
+        var client = ProgramTest.NewClient;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
         var jsonContent = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
-        var response = await ProgramTest.NewClient.PostAsync("api/coupons", jsonContent);
-    }
-
-    Task IAsyncLifetime.DisposeAsync()
-    {
-        return Task.Run(() => Console.WriteLine("yay"));
-    }
-
-    Task IAsyncLifetime.InitializeAsync()
-    {
-        return Task.Run(addCouponBeforeTest);
+        var response = await client.PostAsync("api/coupon", jsonContent);
+        var stringResponse = await response.Content.ReadAsStringAsync();
+        var model = stringResponse.FromJson<GetByIdCouponResponse>();
     }
 }
